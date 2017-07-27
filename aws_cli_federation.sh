93f7dc82cc044e00d09749a9595f649ea9548599
Jesse Osiecki <jesse.osiecki@solarwinds.com>
2017-07-27 12:57:47 -0400

    easy bash wrapper

---
+++ aws_cli_federation.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+get_script_dir () {
+     SOURCE="${BASH_SOURCE[0]}"
+     # While $SOURCE is a symlink, resolve it
+     while [ -h "$SOURCE" ]; do
+          DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+          SOURCE="$( readlink "$SOURCE" )"
+          # If $SOURCE was a relative symlink (so no "/" as prefix, need to resolve it relative to the symlink base directory
+          [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+     done
+     DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+     echo "$DIR"
+}
+
+source $(get_script_dir)/bin/activate
+python $(get_script_dir)/aws_cli_federation.py
